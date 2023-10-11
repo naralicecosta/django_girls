c46285d50e9f526ea232566a7cca74eb425c798d
naralicecosta <naralicecosta04@gmail.com>
2023-10-11 09:31:13 -0300

    feat: adicionando html no site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+#  criamos uma função (def) chamada post_list que leva a solicitação e irá retornar o valor
+#  que recebe ao chamar outra função render que irá renderizar (montar) nosso modelo blog/post_list.html.
